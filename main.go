7673b0cab7a9345487f2af849748c4ea2c87706f
Ano2225 <ouatt0767@gmail.com>
2025-02-05 00:07:39 +0000

    renamed the files for better understand

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	// Initialise Cobra(bibliotheque utilisée pour ecrire les commandes en cli) et verifie qu'il n'y a pas d'erreur
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
